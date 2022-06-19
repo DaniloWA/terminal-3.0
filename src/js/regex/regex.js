93c2cc32e7d998cb9a779486899894c07fbb4367
Danilo <daniloworkdev@gmail.com>
2022-06-19 13:04:02 +0100

    feat: implementado expressão regular do comando gerar senha

--- src/js/regex/regex.js
+++ src/js/regex/regex.js
@@ -2,3 +2,8 @@ export function calculadoraRG(){
     let regExp = /(\d+) (mais|\+|\-|\*|\/|menos|dividido|por|vezes) (\d+)/i
     return regExp
 }
+
+export function regexGerarSenha() {
+    let regExp = /gerar senhas?\s?(\d+)?\s?(com|sem)?/i
+    return regExp
+}
