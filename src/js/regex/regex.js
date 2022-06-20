5f831671c206fc37da4355c34cebc398e155de02
Danilo <daniloworkdev@gmail.com>
2022-06-20 13:59:26 +0100

    feat: implementado regex para função embaralhar texto

--- src/js/regex/regex.js
+++ src/js/regex/regex.js
@@ -1,16 +1,16 @@
-// expressão regular para Calculadora
+// Calculadora
 export function calculadoraRG(){
     let regExp = /(\d+) (mais|\+|\-|\*|\/|menos|dividido|por|vezes) (\d+)/i
     return regExp
 }
 
-// expressão regular para Senha
+// Senha
 export function regexGerarSenha() {
     let regExp = /gerar senhas?\s?(\d+)?\s?(com|sem)?/i
     return regExp
 }
 
-// expressão regular para Analisador de texto
+// Analisador de texto
 export function analisarTxt() {
     let regExp = /^analisar texto/i
     return regExp
@@ -23,5 +23,11 @@ export function contaPalavras() {
 
 export function contaCaracteres() {
     let regExp = /\w/ig
+    return regExp
+}
+
+// Embaralhar o texto
+export function regexEmbaralharTexto() {
+    let regExp = /embaralhar texto/i
     return regExp
 }
